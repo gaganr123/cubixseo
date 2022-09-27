a2de1aa3a86089efc584e8303879b0c5c16e1a6a
gaganr123 <gngxrawat@gmail.com>
2022-09-27 08:14:46 +0545

    gf

--- footer.js
+++ footer.js
@@ -8,7 +8,7 @@ class MyFooter extends HTMLElement {
             <div class="row gy-4">
               <div class="col-lg-5 col-md-12 footer-info">
                 <a href="index.html" class="logo d-flex align-items-center">
-                  <span>Impact</span>
+                  <span>Stair</span>
                 </a>
                 <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
                 <div class="social-links d-flex mt-4">
@@ -22,7 +22,7 @@ class MyFooter extends HTMLElement {
               <div class="col-lg-2 col-6 footer-links">
                 <h4>Useful Links</h4>
                 <ul>
-                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Why Choose Us</a></li>
                   <li><a href="#">About us</a></li>
                   <li><a href="#">Services</a></li>
                   <li><a href="#">Terms of service</a></li>
@@ -33,19 +33,19 @@ class MyFooter extends HTMLElement {
               <div class="col-lg-2 col-6 footer-links">
                 <h4>Our Services</h4>
                 <ul>
-                  <li><a href="#">Web Design</a></li>
+                  <li><a href="#">Digital Marketing</a></li>
                   <li><a href="#">Web Development</a></li>
-                  <li><a href="#">Product Management</a></li>
-                  <li><a href="#">Marketing</a></li>
-                  <li><a href="#">Graphic Design</a></li>
+                  <li><a href="#">Web Hosting</a></li>
+                  <li><a href="#">SEO Content Writing</a></li>
+                  <li><a href="#">SEO</a></li>
                 </ul>
               </div>
       
               <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
                 <h4>Contact Us</h4>
                 <p>
-                  A108 Adam Street <br>
-                  New York, NY 535022<br>
+                  2109 Paradise Ridge Dr<br>
+                  Texas<br>
                   United States <br><br>
                   <strong>Phone:</strong> +1 5589 55488 55<br>
                   <strong>Email:</strong> info@example.com<br>
@@ -58,7 +58,7 @@ class MyFooter extends HTMLElement {
       
           <div class="container mt-4">
             <div class="copyright">
-              &copy; Copyright <strong><span>Impact</span></strong>. All Rights Reserved
+              &copy; Copyright <strong><span>Stair</span></strong>. All Rights Reserved
             </div>
           
           </div>
